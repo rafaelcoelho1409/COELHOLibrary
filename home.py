4749f6bb848cc544644ee3c578345085e0b6b85b
rafaelcoelho1409 <rafaelcoelho1409@hotmail.com>
2024-07-17 14:18:15 -0300

    Test

--- home.py
+++ home.py
@@ -5,29 +5,29 @@ import subprocess
 
 st.set_page_config(layout="wide")
 
-#st.markdown(
-#    """
-#    <style>
-#    .css-18e3th9 {
-#        padding: 0;
-#    }
-#    .css-1d391kg {
-#        padding: 0;
-#        margin: 0;
-#    }
-#    .main .block-container {
-#        padding: 0;
-#    }
-#    iframe {
-#        width: 100%;
-#        height: calc(100vh - 32px); /* Set height to full viewport minus title height */
-#        border: none;
-#        overflow: auto;
-#    }
-#    </style>
-#    """,
-#    unsafe_allow_html=True
-#)
+st.markdown(
+    """
+    <style>
+    .css-18e3th9 {
+        padding: 0;
+    }
+    .css-1d391kg {
+        padding: 0;
+        margin: 0;
+    }
+    .main .block-container {
+        padding: 0;
+    }
+    iframe {
+        width: 100%;
+        height: calc(100vh - 32px); /* Set height to full viewport minus title height */
+        border: none;
+        overflow: auto;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 def run_dash():
     #command = """
