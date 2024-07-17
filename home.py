ac2923e66389f39677aa61d2652ebfa22fc5c3fe
rafaelcoelho1409 <rafaelcoelho1409@hotmail.com>
2024-07-17 14:12:48 -0300

    Test

--- home.py
+++ home.py
@@ -5,29 +5,29 @@ import subprocess
 
 st.set_page_config(layout="wide")
 
-st.markdown(
-    """
-    <style>
-    .css-18e3th9 {
-        padding: 0;
-    }
-    .css-1d391kg {
-        padding: 0;
-        margin: 0;
-    }
-    .main .block-container {
-        padding: 0;
-    }
-    iframe {
-        width: 100%;
-        height: calc(100vh - 32px); /* Set height to full viewport minus title height */
-        border: none;
-        overflow: auto;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+#st.markdown(
+#    """
+#    <style>
+#    .css-18e3th9 {
+#        padding: 0;
+#    }
+#    .css-1d391kg {
+#        padding: 0;
+#        margin: 0;
+#    }
+#    .main .block-container {
+#        padding: 0;
+#    }
+#    iframe {
+#        width: 100%;
+#        height: calc(100vh - 32px); /* Set height to full viewport minus title height */
+#        border: none;
+#        overflow: auto;
+#    }
+#    </style>
+#    """,
+#    unsafe_allow_html=True
+#)
 
 def run_dash():
     #command = """
@@ -47,4 +47,4 @@ def run_dash():
 dash_thread = threading.Thread(target = run_dash)
 dash_thread.start()
 
-st.components.v1.iframe("http://127.0.0.1:8050", width = None, height = None, scrolling = True)
+st.components.v1.iframe("http://127.0.0.1:8050", width = 500, height = 500, scrolling = True)
